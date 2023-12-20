678f5bde5084706bf1a745089c3525ac5feebc87
liyuan3210 <li.yuan3210@163.com>
2023-12-20 11:09:15 +0800

    commit debug

---
+++ spring-framework-6.0.7/spring-debug/src/main/java/org/springframework/debug/MyTest1.java
@@ -0,0 +1,45 @@
+package org.springframework.debug;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+class User {
+	private Integer id;
+	private String userName;
+	// 下面就是setter、getter、toString和构造器，此处省略
+	public User(Integer id, String userName) {
+		this.id = id;
+		this.userName = userName;
+	}
+}
+
+@Configuration
+@Component
+class JavaConfig {
+	@Bean
+	public User user(){
+		return new User(1, "psj");
+	}
+}
+
+public class MyTest1 {
+	public static void main(String[] args) {
+		System.out.println("--------into start");
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
+		User user = (User) context.getBean("user");
+		System.out.println(user.toString());  // 可以成功输出就OK了！！！
+	}
+}
+/**
+build.gradle配置
+ //引用本地模块源码1
+ //    implementation project(path: ':spring-context')
+ //引用本地模块源码2
+ //    optional(project(":spring-context"))
+ //引用本地模块源码3
+ api(project(":spring-context"))
+ //引用远程仓库jar
+ //    optional("org.aspectj:aspectjweaver")
+ */
